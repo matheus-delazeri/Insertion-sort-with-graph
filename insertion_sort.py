0a70d8e8d091a170b90956f4f1a34715a0d54b55
matheus <matheusdelazeris@gmail.com>
2021-06-18 23:54:32 -0300

    added graph-off parameter

--- insertion_sort.py
+++ insertion_sort.py
@@ -3,17 +3,21 @@ import matplotlib.pyplot as plt
 import sys
 
 ARRAY_SIZE = int(sys.argv[1])
+graph_on = True
+if len(sys.argv) >= 3:
+    graph_on = False if sys.argv[2]=='graph-off' else True
 np.set_printoptions(precision=3, suppress=True)
 plt.rcParams['toolbar'] = 'None'
 
-plt.ion()
-fig = plt.figure()
-fig.canvas.manager.set_window_title('Insert Sort')
-ax = fig.add_subplot(111)
-
 num_array = np.random.rand(ARRAY_SIZE)
 len_array = np.arange(ARRAY_SIZE)
-line, = ax.plot(len_array, num_array, '-r')
+
+if(graph_on):
+    plt.ion()
+    fig = plt.figure()
+    fig.canvas.manager.set_window_title('Insert Sort')
+    ax = fig.add_subplot(111)
+    line, = ax.plot(len_array, num_array, '-r')
 
 print("Initial array:")
 print(num_array)
@@ -27,13 +31,14 @@ for i in range(len(num_array)):
             num_array[j] = temp
             i -= 1
         j -= 1
-    line.set_ydata(num_array)
-    fig.canvas.draw()
-    fig.canvas.flush_events()
+    if(graph_on):
+        line.set_ydata(num_array)
+        fig.canvas.draw()
+        fig.canvas.flush_events()
 
 print("\nSorted array:")
 print(num_array,"\n")
 
-while True:
+while graph_on:
     fig.canvas.flush_events()
     fig.canvas.mpl_connect('close_event', sys.exit)
